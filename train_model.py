497ea3c8ad17cf78531c03c4d82a856865b4f344
Damia Fatihah <93441486+damiafatihah@users.noreply.github.com>
2023-11-05 10:46:53 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -5,24 +5,10 @@
 ## This program is licenced under the BSD 2-Clause licence,
 ## contained in the LICENCE file in this directory.
 
-import os
-import pathlib
-import pandas as pd
-import numpy as np
 import tensorflow as tf
-import random
-import cv2
-import matplotlib.pyplot as plt
-from tensorflow import keras
-from PIL import Image
 from setup_model import GTSRB
-from matplotlib.image import imread
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img, load_img
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.optimizers.legacy import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout, BatchNormalization
 
 def train(data, file_name, num_epochs=30, batch_size=32, IMG_HEIGHT=30, IMG_WIDTH=30, channels=3, init=None):
